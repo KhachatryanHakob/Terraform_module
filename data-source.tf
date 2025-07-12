a4bbf678d3f666f956f7a1c71a9823b3e45e6de3
Hakob <hakob.kh.2003@gmail.com>
2025-07-12 21:55:11 +0400

    last changes

--- data-source.tf
+++ data-source.tf
@@ -2,15 +2,16 @@ data "aws_availability_zones" "az" {}
 
 data "aws_ami" "latest_ubuntu24" {
   most_recent = true
-  owners      = ["099720109477"] 
+  owners      = ["099720109477"]  # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
 data "aws_vpc" "default" {
   default = true
 }
+
 
